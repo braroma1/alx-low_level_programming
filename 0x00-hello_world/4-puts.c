5ba7c734ea759d7b08a8a345338d3baa4010c5f7
braroma1 <ahonibent@gmail.com>
2023-02-16 18:29:25 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#!bin/bash
-#include <stdio .h>
-int main(void){
+#include <stdio.h>
+/**
+ *main- A program that uses puts to print a string
+ *Return: ends with 0
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
